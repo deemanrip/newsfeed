9e308504313f5f64b8388f914645cf6b495cecfd
Dmitry Yukhlin <deemansm@gmail.com>
2019-02-22 21:14:45 +0400

    Refactoring of creation article table script

--- src/main/resources/db/migration/V1__create_article_table.sql
+++ src/main/resources/db/migration/V1__create_article_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS article (
   id BIGINT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(255),
+  title VARCHAR(255) NOT NULL,
   description VARCHAR(1000),
-  publication_date DATE,
-  extraction_url TEXT,
-  main_image_url TEXT,
-  source_url TEXT,
+  publication_date DATE NOT NULL,
+  extraction_link TEXT NOT NULL,
+  main_image_link TEXT NOT NULL,
+  source_link TEXT NOT NULL,
 
-  PRIMARY KEY ( id )
+  CONSTRAINT pk_article_id PRIMARY KEY (id)
 )
