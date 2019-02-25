e0f70f2af5953e18c56466f2817140c4880dab95
Dmitry Yukhlin <deemansm@gmail.com>
2019-02-25 22:41:53 +0400

    Added rest controller

---
+++ src/main/java/com/yukhlin/newsfeed/controller/CrawlerController.java
@@ -0,0 +1,38 @@
+package com.yukhlin.newsfeed.controller;
+
+import com.yukhlin.newsfeed.data.entity.Article;
+import com.yukhlin.newsfeed.service.CrawlerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.stream.XMLStreamException;
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/crawler")
+public class CrawlerController {
+
+    private CrawlerService crawlerService;
+
+    @Autowired
+    public CrawlerController(CrawlerService crawlerService) {
+        this.crawlerService = crawlerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> crawlSites() throws XMLStreamException, IOException {
+        crawlerService.crawlSites();
+        return new ResponseEntity<>("Sites crawled successfully", HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Article>> getCrawlerResult() {
+        return new ResponseEntity<>(crawlerService.getCrawlingResult(), HttpStatus.OK);
+    }
+}
